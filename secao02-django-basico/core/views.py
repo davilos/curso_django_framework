ff0923074c2c9139a9d30338b57e4f33038cf5a5
davilos <daviloscostagg@hotmail.com>
2022-10-10 12:03:48 -0300

    changed files

--- secao02-django-basico/core/views.py
+++ secao02-django-basico/core/views.py
@@ -4,7 +4,7 @@ from django.shortcuts import render
 def index(request):
     context = {
         'curso': 'Programação Web com Django Framework',
-        'outro': 'Django é massa!'
+        'outro': 'Django é massa!',
     }
     return render(request, 'index.html', context)
 
