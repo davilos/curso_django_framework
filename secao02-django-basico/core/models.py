d6a2f852762d08e3c5d21837e97fb11afd62975e
davilos <daviloscostagg@hotmail.com>
2022-10-10 20:06:23 -0300

    added classes and using the admin in admin.py

--- secao02-django-basico/core/models.py
+++ secao02-django-basico/core/models.py
@@ -6,8 +6,14 @@ class Produto(models.Model):
     preco = models.DecimalField('Preço', decimal_places=2, max_digits=8)
     estoque = models.IntegerField('Quantidade em Estoque')
 
+    def __str__(self) -> str:
+        return self.nome
+
 
 class Cliente(models.Model):
     nome = models.CharField('Nome', max_length=100)
     sobrenome = models.CharField('Sobrenome', max_length=100)
     email = models.EmailField('E-mail', max_length=100)
+
+    def __str__(self) -> str:
+        return f'{self.nome} {self.sobrenome}'
