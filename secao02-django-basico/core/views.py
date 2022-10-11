acf894f2a22d1270660e13fb0f81f45fb707b76b
davilos <daviloscostagg@hotmail.com>
2022-10-11 13:13:07 -0300

    using the django shell and testing it

--- secao02-django-basico/core/views.py
+++ secao02-django-basico/core/views.py
@@ -2,12 +2,23 @@ from django.shortcuts import render
 
 
 def index(request):
+    print(dir(request))
+    print(dir(request.user))
+
+    if request.user.is_anonymous:
+        teste = 'Usuário não logado'
+    else:
+        teste = 'Usuário logado'
+
     context = {
         'curso': 'Programação Web com Django Framework',
         'outro': 'Django é massa!',
+        'logado': teste
     }
     return render(request, 'index.html', context)
 
 
 def contact(request):
-    return render(request, 'contact.html')
+    if request.user.is_staff:
+        return render(request, 'contact.html')
+    return render(request, 'permissions.html')
