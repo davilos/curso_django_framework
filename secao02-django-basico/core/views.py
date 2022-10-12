cc7002d1f32cebfc6d0aa23be8822676e147bad2
davilos <daviloscostagg@hotmail.com>
2022-10-12 14:57:49 -0300

    static files in Django

--- secao02-django-basico/core/views.py
+++ secao02-django-basico/core/views.py
@@ -1,5 +1,7 @@
-from django.shortcuts import render
+from django.shortcuts import render, get_object_or_404
 from .models import Produto
+from django.http import HttpResponse
+from django.template import loader
 
 
 def index(request):
@@ -20,8 +22,22 @@ def contact(request):
 
 
 def produto(request, pk):
-    prod = Produto.objects.get(id=pk)
+    # prod = Produto.objects.get(id=pk)
+    prod = get_object_or_404(Produto, id=pk)
+
     context = {
         'prod': prod
     }
     return render(request, 'produto.html', context)
+
+
+def error404(request, ex):
+    template = loader.get_template('404.html')
+
+    return HttpResponse(content=template.render(), content_type='text/html; charset=utf-8', status=404)
+
+
+def error500(request):
+    template = loader.get_template('500.html')
+
+    return HttpResponse(content=template.render(), content_type='text/html; charset=utf-8', status=500)
--- secao02-django-basico/django1/settings.py
+++ secao02-django-basico/django1/settings.py
@@ -25,6 +25,8 @@ SECRET_KEY = (
 )
 
 # SECURITY WARNING: don't run with debug turned on in production!
+# DEBUG com valor True = Modo de desenvolvimento
+# DEBUG com valor False = Modo de produção
 DEBUG = True
 
 ALLOWED_HOSTS = ['*']
@@ -118,9 +120,12 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/4.1/howto/static-files/
 
-STATIC_URL = 'static/'
+STATIC_URL = 'static/' # Usado durante o desenvolvimento
+STATIC_ROOT = BASE_DIR / 'staticfiles' # Usado durante a produção
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/4.1/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+
+LOGOUT_REDIRECT_URL = 'index'
