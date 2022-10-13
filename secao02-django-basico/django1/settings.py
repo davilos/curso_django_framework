53af51c1a5f95b52e929c8b9be3d04a914470881
davilos <daviloscostagg@hotmail.com>
2022-10-13 10:22:34 -0300

    finished project

--- secao02-django-basico/django1/settings.py
+++ secao02-django-basico/django1/settings.py
@@ -27,9 +27,9 @@ SECRET_KEY = (
 # SECURITY WARNING: don't run with debug turned on in production!
 # DEBUG com valor True = Modo de desenvolvimento
 # DEBUG com valor False = Modo de produção
-DEBUG = True
+DEBUG = False
 
-ALLOWED_HOSTS = ['*']
+ALLOWED_HOSTS = ['*'] 
 
 
 # Application definition
@@ -46,6 +46,7 @@ INSTALLED_APPS = [
 
 MIDDLEWARE = [
     'django.middleware.security.SecurityMiddleware',
+    'whitenoise.middleware.WhiteNoiseMiddleware',
     'django.contrib.sessions.middleware.SessionMiddleware',
     'django.middleware.common.CommonMiddleware',
     'django.middleware.csrf.CsrfViewMiddleware',
